ef6c9c3c8ec83c62732b9c5d55fe3a99e0e8642c
Ale-253 <ale.santamarina@hotmail.com>
2023-12-03 15:33:35 -0300

    fix

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,152 +1,133 @@
-const path = require('path');
-const db = require('../database/models');
+const path = require("path");
+const db = require("../database/models");
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
-const moment = require('moment');
-const { getAllMovies } = require('../services/movies_services-js');
 
-
-
-
-//Aqui tienen otra forma de llamar a cada uno de los modelos
-const Movies = db.Movie;
-const Genres = db.Genre;
-const Actors = db.Actor;
-
+const createError = require('http-errors');
+const paginate = require('express-paginate')
+const { getAllMovis, getMovieById, createMovie, updateMovie, deleteMovie } = require("../services/movies.services");
 
 const moviesController = {
-    'list': async (req, res) => {
-        try {
-            const movies = await getAllMovies
+  list: async (req, res) => {
+    try {
+      const { movies, total } = await getAllMovis(req.query.limit, req.skip);
+      const pagesCount = Math.ceil(total / req.query.limit)
+      const currentPage = req.query.page;
+      const pages = paginate.getArrayPages(req)(pagesCount, pagesCount, currentPage)
 
-            return res.status(200).json({
-                ok: true,
-                data: movies
-            })
-        }
-        catch (error) {
-            return res.status(error.status || 500).json({
-                ok: false,
-                status: error.status || 500,
-                error: error.message || 'Ups, hubo un error :('
-            })
-        }
+      return res.status(200).json({
+        ok: true,
+        meta: {
+          total,
+          pagesCount,
+          currentPage,
+          pages
+        },
+        data: movies,
+      });
 
-    },
-    'detail': (req, res) => {
-        db.Movie.findByPk(req.params.id,
-            {
-                include: ['genre']
-            })
-            .then(movie => {
-                res.render('moviesDetail.ejs', { movie });
-            });
-    },
-    'new': (req, res) => {
-        db.Movie.findAll({
-            order: [
-                ['release_date', 'DESC']
-            ],
-            limit: 5
-        })
-            .then(movies => {
-                res.render('newestMovies', { movies });
-            });
-    },
-    'recomended': (req, res) => {
-        db.Movie.findAll({
-            include: ['genre'],
-            where: {
-                rating: { [db.Sequelize.Op.gte]: 8 }
-            },
-            order: [
-                ['rating', 'DESC']
-            ]
-        })
-            .then(movies => {
-                res.render('recommendedMovies.ejs', { movies });
-            });
-    },
-    //Aqui dispongo las rutas para trabajar con el CRUD
-    add: function (req, res) {
-        let promGenres = Genres.findAll();
-        let promActors = Actors.findAll();
-
-        Promise
-            .all([promGenres, promActors])
-            .then(([allGenres, allActors]) => {
-                return res.render(path.resolve(__dirname, '..', 'views', 'moviesAdd'), { allGenres, allActors })
-            })
-            .catch(error => res.send(error))
-    },
-    create: function (req, res) {
-        Movies
-            .create(
-                {
-                    title: req.body.title,
-                    rating: req.body.rating,
-                    awards: req.body.awards,
-                    release_date: req.body.release_date,
-                    length: req.body.length,
-                    genre_id: req.body.genre_id
-                }
-            )
-            .then(() => {
-                return res.redirect('/movies')
-            })
-            .catch(error => res.send(error))
-    },
-    edit: function (req, res) {
-        let movieId = req.params.id;
-        let promMovies = Movies.findByPk(movieId, { include: ['genre', 'actors'] });
-        let promGenres = Genres.findAll();
-        let promActors = Actors.findAll();
-        Promise
-            .all([promMovies, promGenres, promActors])
-            .then(([Movie, allGenres, allActors]) => {
-                Movie.release_date = moment(Movie.release_date).format('L');
-                return res.render(path.resolve(__dirname, '..', 'views', 'moviesEdit'), { Movie, allGenres, allActors })
-            })
-            .catch(error => res.send(error))
-    },
-    update: function (req, res) {
-        let movieId = req.params.id;
-        Movies
-            .update(
-                {
-                    title: req.body.title,
-                    rating: req.body.rating,
-                    awards: req.body.awards,
-                    release_date: req.body.release_date,
-                    length: req.body.length,
-                    genre_id: req.body.genre_id
-                },
-                {
-                    where: { id: movieId }
-                })
-            .then(() => {
-                return res.redirect('/movies')
-            })
-            .catch(error => res.send(error))
-    },
-    delete: function (req, res) {
-        let movieId = req.params.id;
-        Movies
-            .findByPk(movieId)
-            .then(Movie => {
-                return res.render(path.resolve(__dirname, '..', 'views', 'moviesDelete'), { Movie })
-            })
-            .catch(error => res.send(error))
-    },
-    destroy: function (req, res) {
-        let movieId = req.params.id;
-        Movies
-            .destroy({ where: { id: movieId }, force: true }) // force: true es para asegurar que se ejecute la acción
-            .then(() => {
-                return res.redirect('/movies')
-            })
-            .catch(error => res.send(error))
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        ok: false,
+        status: error.status || 500,
+        error: error.message || "Upss, hubo un error :(",
+      });
     }
-}
+  },
+  detail: async (req, res) => {
+    try {
+      const movie = await getMovieById(req.params.id);
+
+      return res.status(200).json({
+        ok: true,
+        data: movie,
+      });
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        ok: false,
+        status: error.status || 500,
+        error: error.message || "Upss, hubo un error :(",
+      });
+    }
+  },
+  create: async (req, res) => {
+    try {
+
+      const { title, release_date, awards, rating, length, genre_id, actors } = req.body;
+
+
+      if ([title, release_date, awards, rating].includes('' || undefined)) {
+        throw createError(400, 'Los campos title, release_date, awards, rating son obligatorios')
+      }
+
+      const newMovie = await createMovie({
+        title,
+        rating,
+        awards,
+        release_date,
+        length,
+        genre_id
+      },
+        actors
+      );
+
+      return res.status(200).json({
+        ok: true,
+        data: 'Pelicula creada con exito',
+        url: `${req.protocol}://${req.get('host')}/api/v1/movies/${newMovie.id}`
+      });
+
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        ok: false,
+        status: error.status || 500,
+        error: error.message || "Upss, hubo un error :(",
+      });
+    }
+
+  },
+  update: async (req, res) => {
+
+    try {
+
+      const movieUpdated = await updateMovie(req.params.id, req.body);
+
+      return res.status(200).json({
+        ok: true,
+        data: 'Pelicula actualizada con exito',
+        url: `${req.protocol}://${req.get('host')}/api/v1/movies/${req.params.id}`
+      });
+
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        ok: false,
+        status: error.status || 500,
+        error: error.message || "Upss, hubo un error :(",
+      });
+    }
+
+  },
+
+  destroy: async (req, res) => {
+    try {
+
+      const movieUpdate = await deleteMovie(req.params.id, req.body);
+
+
+      return res.status(200).json({
+        ok: true,
+        data: 'Pelicula eliminada con exito',
+      });
+
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        ok: false,
+        status: error.status || 500,
+        error: error.message || "Upss, hubo un error :(",
+      });
+
+    }
+  }
+};
 
 module.exports = moviesController;
--- src/routes/api.v1/movies.routes.js
+++ src/routes/api.v1/movies.routes.js
@@ -1,9 +1,15 @@
-const { list, detail } = require('../../controllers/moviesController')
+const { list, detail, create, update, destroy } = require('../../controllers/moviesController')
+
 const router = require('express').Router()
 
 /* /api/v1/movies */
 
-router.get('/', list)
-router.get('/:id', detail)
+router
+    .get('/', list)
+    .get('/:id', detail)
+    .post('/', create)
+    .put('/:id', update)
+    .delete('/:id', destroy)
+
 
 module.exports = router
